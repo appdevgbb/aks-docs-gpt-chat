4374273a6148690e01b919d7d0a1f2dee4676e48
Ray Kao <ray@peopleandcode.com>
2023-10-03 19:49:05 -0400

    update: model will auto generate guid

--- src/HttpSummurization/models/summary.cs
+++ src/HttpSummurization/models/summary.cs
@@ -4,15 +4,17 @@ namespace HttpSummarization.Models
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
-        public string Summary { get; set; }
+        public string SummaryText { get; set; }
+        public string OriginalText{ get; set;}
         public string ArticleUrl { get; set; }
-        public Dictionary<string, string> Links { get; set; }
+        public Dictionary<string, string>? Links { get; set; }
 
-        public Summary(Guid id, string title, string summary, string articleUrl, Dictionary<string, string> links)
+        public Summary(string title, string summaryText, string originalText, string articleUrl, Dictionary<string, string>? links)
         {
-            this.Id = id;
+            this.Id = Guid.NewGuid();
             this.Title = title;
-            this.Summary = summary;
+            this.SummaryText = summaryText;
+            this.OriginalText = originalText;
             this.ArticleUrl = articleUrl;
             this.Links = links;
         }
